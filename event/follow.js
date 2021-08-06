2762735767480f47329502eb6d5f3894731e1e4f
unyon <sybil.zap05@gmail.com>
2021-08-06 22:32:49 +0900

    message

--- event/follow.js
+++ event/follow.js
@@ -3,7 +3,7 @@ exports.index = () => {
   // 返信するメッセージを作成
   const message = {
     type: 'text',
-    text: '友達追加ありがとうございます！',
+    text: '友達追加ありがとうございます！このLINE Botの使用方法を確認する際は、「使用方法」と入力してください。',
   };
   // 返信するメッセージをこの関数の呼び出し元（bot.js）に返す
   return message;
